06d74ce72f1df9fc575947f4a296f5cfa7363327
kimroutley <kimalex.routley89@gmail.com>
2026-02-18 01:18:32 +1100

    feat(cb_theme): add indexed handbook content and wire guide handbook tab

--- packages/cb_theme/lib/src/widgets.dart
+++ packages/cb_theme/lib/src/widgets.dart
@@ -29,3 +29,4 @@ export 'widgets/cb_status_rail.dart';
 export 'widgets/cb_prism_scaffold.dart';
 export 'widgets/cb_alliance_graph.dart';
 export 'widgets/cb_phase_timeline.dart';
+export 'widgets/handbook_content.dart';
