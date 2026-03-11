9ba4d0f24800e0c0131cdee16a6a41cb47a1b49c
kimroutley <kimroutley@users.noreply.github.com>
2026-03-11 22:26:34 +1100

    test: Fix widget smoke test.
    - widget_test.dart: Injected mock implementation of SharedPreferences into sharedPrefsProvider before spinning up the PlayerApp to resolve unhandled MissingPluginException and uninitialized Provider errors.

--- apps/player/test/widget_test.dart
+++ apps/player/test/widget_test.dart
@@ -2,11 +2,14 @@ import 'dart:ui';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:cb_player/main.dart';
+import 'package:cb_player/shared_prefs_provider.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.setMockInitialValues({});
 
   testWidgets('Player app smoke test', (WidgetTester tester) async {
     tester.view.physicalSize = const Size(1080, 1920);
@@ -14,6 +17,13 @@ void main() {
     addTearDown(tester.view.resetPhysicalSize);
     addTearDown(tester.view.resetDevicePixelRatio);
 
-    await tester.pumpWidget(const ProviderScope(child: PlayerApp()));
+    final prefs = await SharedPreferences.getInstance();
+
+    await tester.pumpWidget(ProviderScope(
+      overrides: [
+        sharedPrefsProvider.overrideWithValue(prefs),
+      ],
+      child: const PlayerApp(),
+    ));
   });
 }
