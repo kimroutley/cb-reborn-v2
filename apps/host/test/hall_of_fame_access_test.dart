f7fcb1c6a01a6f816a1f863bb82b6c62c1d613aa
kimroutley <kimroutley@users.noreply.github.com>
2026-03-11 22:34:35 +1100

    test: Fix host hall of fame access test.
    - hall_of_fame_access_test.dart: Added authenticated AuthState stub to bypass the HostAuthScreen on the 'VIEW HALL OF FAME' action in the host home screen.

--- apps/host/test/hall_of_fame_access_test.dart
+++ apps/host/test/hall_of_fame_access_test.dart
@@ -2,9 +2,11 @@ import 'package:cb_host/host_destinations.dart';
 import 'package:cb_host/host_navigation.dart';
 import 'package:cb_host/screens/home_screen.dart';
 import 'package:cb_logic/cb_logic.dart';
+import 'package:cb_host/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hive_ce/hive.dart';
 
 class _FakeBox extends Fake implements Box<String> {
@@ -24,6 +26,20 @@ class _FakeBox extends Fake implements Box<String> {
   bool containsKey(key) => false;
 }
 
+class _StubAuthNotifier extends AuthNotifier {
+  _StubAuthNotifier(this.initial);
+
+  final AuthState initial;
+
+  @override
+  AuthState build() => initial;
+}
+
+class _FakeUser implements User {
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
 void main() {
   setUp(() {
     PersistenceService.initWithBoxes(_FakeBox(), _FakeBox(), _FakeBox());
@@ -31,7 +47,14 @@ void main() {
 
   testWidgets('Home quick action opens Hall of Fame destination',
       (WidgetTester tester) async {
-    final container = ProviderContainer();
+    final container = ProviderContainer(
+      overrides: [
+        authProvider.overrideWith(
+          () => _StubAuthNotifier(
+              AuthState(AuthStatus.authenticated, user: _FakeUser())),
+        ),
+      ],
+    );
     addTearDown(container.dispose);
 
     await tester.pumpWidget(
